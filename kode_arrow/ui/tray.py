0f4ffc7718c40618eba2615cdf8b50fbda60049a
Ahmad-Hassan-0 <123323526+Ahmad-Hassan-0@users.noreply.github.com>
2026-03-01 15:32:16 +0500

    fix: make tray menu dynamically evaluate premium status on every click

--- kode_arrow/ui/tray.py
+++ kode_arrow/ui/tray.py
@@ -15,35 +15,35 @@ class SystemTray:
     def build_menu(
         self,
         *,
-        is_premium: bool,
-        on_unlock: Optional[Callable[[], None]],
+        is_premium_fn: Callable[[], bool],
+        on_unlock: Callable[[], None],
         on_exit: Callable[Any, None],
         on_open_portfolio: Callable[[], None],
         on_open_website: Callable[[], None],
     ) -> None:
         img = Image.open(self._icon_path).resize((16, 16))
 
-        menu_items = [
-            pystray.MenuItem('Created by Ahmad Hassan', on_open_portfolio),
-        ]
+        def create_menu_items():
+            menu_items = [
+                pystray.MenuItem('Created by Ahmad Hassan', on_open_portfolio),
+            ]
 
-        if is_premium:
-            menu_items.append(pystray.MenuItem('Visit KodeArrow', on_open_website, default=True))
-        else:
-            menu_items.append(pystray.MenuItem('Buy here for $2', on_open_website, default=True))
-            if on_unlock is not None:
-                # pystray wraps callback in lambda (icon, item): ... 
-                # We need to adapt it since on_unlock takes no args.
-                def wrap_unlock(icon, item):
-                    on_unlock()
-                menu_items.append(pystray.MenuItem('Already bought? Unlock Here', wrap_unlock))
+            if is_premium_fn():
+                menu_items.append(pystray.MenuItem('Visit KodeArrow', on_open_website, default=True))
+            else:
+                menu_items.append(pystray.MenuItem('Buy here for $2', on_open_website, default=True))
+                if on_unlock is not None:
+                    def wrap_unlock(icon, item):
+                        on_unlock()
+                    menu_items.append(pystray.MenuItem('Already bought? Unlock Here', wrap_unlock))
 
-        def wrap_exit(icon, item):
-            on_exit()
-            
-        menu_items.append(pystray.MenuItem('Exit', wrap_exit))
+            def wrap_exit(icon, item):
+                on_exit()
+                
+            menu_items.append(pystray.MenuItem('Exit', wrap_exit))
+            return menu_items
 
-        self._icon = pystray.Icon("KodeArrow by Ahmad Hassan", img, "KodeArrow by Ahmad Hassan", pystray.Menu(*menu_items))
+        self._icon = pystray.Icon("KodeArrow by Ahmad Hassan", img, "KodeArrow by Ahmad Hassan", pystray.Menu(create_menu_items))
 
     def run(self) -> None:
         if self._icon is not None:
